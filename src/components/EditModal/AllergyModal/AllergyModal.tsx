e50b2f1fdc0726d48ffd1fba280426d36d84a67f
elijah newcombe <78599237+enewcombe30@users.noreply.github.com>
2025-11-17 18:28:36 +0000

    bg updated when item selected

--- src/components/EditModal/AllergyModal/AllergyModal.tsx
+++ src/components/EditModal/AllergyModal/AllergyModal.tsx
@@ -29,7 +29,9 @@ export default function AllergyModal({
     return (
       <button
         key={allergy.allergenId}
-        className="w-[12rem] font-bold text-lg border border-border-primary py-2 px-4 rounded text-white transition-colors flex items-center space-x-4"
+        className={`w-[12rem] font-bold text-lg border border-border-primary py-2 px-4 rounded ${
+          isSelected ? "bg-border-primary" : ""
+        } text-white transition-colors flex items-center space-x-4`}
         onClick={() => toggleAllergy(allergy)}
       >
         <div
@@ -50,9 +52,10 @@ export default function AllergyModal({
         {allergies.slice(0, 3).map((allergy) => renderAllergyCheckbox(allergy))}
         <button
           key={"allergy-all"}
-          className="w-[12rem] font-bold border border-border-primary py-2 px-4 rounded text-white transition-colors flex items-center space-x-4"
+          className={`w-[12rem] font-bold border border-border-primary py-2 px-4 rounded text-white transition-colors flex items-center space-x-4`}
           onClick={() => handleShowAll()}
         >
+          {/* Add bg colour when allergies from the full list have been added */}
           <span className="text-white font-medium w-fit mx-auto">
             Allergy All...
           </span>
@@ -62,7 +65,7 @@ export default function AllergyModal({
   }
 
   return (
-    <div className="p-6 space-y-4 mb-6 h-fit">
+    <div className="p-6 space-y-4 mb-6 mx-auto h-fit overflow-scroll max-h-80 custom-scrollbar">
       {allergies.map((allergy) => renderAllergyCheckbox(allergy))}
       <button
         className="font-bold text-lg border border-primary-500 py-2 px-4 rounded text-white hover:bg-primary-500 transition-colors"
